ccd5e8a696e8f3d03d9adf1f44912a49b48e4999
Abhishek Kakade <kakade.abhishek2001@gmail.com>
2023-03-03 20:39:40 +0530

    Copyright added

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
-})
+});
+
+app.get("/privacy", function(req, res){
+    res.redirect("/");
+});
+
+app.get("/termsofuse", function(req, res){
+    res.redirect("/");
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000")
